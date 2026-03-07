ff8ec106a8d81b483da7282312ad69a7b9e1b4ad
003saad <sa5581019@gmail.com>
2026-03-08 00:20:02 +0600

    database and some isssues related to this are fixed

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,39 +1,29 @@
-from django.conf import settings
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.contrib.auth.tokens import default_token_generator
+from django.conf import settings
 from django.core.mail import send_mail
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from django.template.loader import render_to_string
-from django.utils.encoding import force_bytes
-from django.utils.http import urlsafe_base64_encode
 
 
 @receiver(post_save, sender=User)
 def send_activation_email(sender, instance, created, **kwargs):
     if created and not instance.is_active and instance.email:
-        uid = urlsafe_base64_encode(force_bytes(instance.pk))
         token = default_token_generator.make_token(instance)
-
-        activation_link = f"http://127.0.0.1:8000/accounts/activate/{uid}/{token}/"
-
-        message = render_to_string(
-            "accounts/activation_email.txt",
-            {
-                "user": instance,
-                "activation_link": activation_link,
-            },
+        activation_url = (
+            f"{settings.FRONTEND_URL}/accounts/activate/{instance.id}/{token}/"
         )
 
-        send_mail(
-            subject="Activate your account",
-            message=message,
-            from_email=settings.DEFAULT_FROM_EMAIL,
-            recipient_list=[instance.email],
-            fail_silently=False,
+        subject = "Activate Your Account"
+        message = (
+            f"Hi {instance.username},\n\n"
+            f"Please activate your account by clicking the link below:\n"
+            f"{activation_url}\n\n"
+            f"Thank You!"
         )
+        recipient_list = [instance.email]
 
-        print("\n" + "=" * 80)
-        print("ACTIVATION LINK:")
-        print(activation_link)
-        print("=" * 80 + "\n")
+        try:
+            send_mail(subject, message, settings.EMAIL_HOST_USER, recipient_list)
+        except Exception as e:
+            print(f"Failed to send email to {instance.email}: {str(e)}")
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,3 +1,22 @@
+# from django.contrib.auth.views import LoginView, LogoutView
+# from django.urls import path
+
+# from .forms import StyledAuthenticationForm
+# from .views import activate_user, signup_view
+
+# urlpatterns = [
+#     path("signup/", signup_view, name="signup"),
+#     path(
+#         "login/",
+#         LoginView.as_view(
+#             template_name="accounts/login.html",
+#             authentication_form=StyledAuthenticationForm,
+#         ),
+#         name="login",
+#     ),
+#     path("logout/", LogoutView.as_view(), name="logout"),
+#     path("activate/<uidb64>/<token>/", activate_user, name="activate_user"),
+# ]
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
@@ -15,5 +34,5 @@ urlpatterns = [
         name="login",
     ),
     path("logout/", LogoutView.as_view(), name="logout"),
-    path("activate/<uidb64>/<token>/", activate_user, name="activate_user"),
+    path("activate/<int:uid>/<str:token>/", activate_user, name="activate_user"),
 ]
